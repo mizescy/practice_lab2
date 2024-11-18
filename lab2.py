7802d8ecbc953a09b0ec804f8656a47526b92c77
Gordon Boo <gordon_boo.23@ichat.sp.edu.sg>
2024-11-18 20:58:11 +0800

    latest lab2

--- lab2.py
+++ lab2.py
@@ -1,1 +1,15 @@
-print("why is dylan stupid")
+def calculate_bmi(height, weight):
+    print("Height= " + str(height))
+    print("Weight= " + str(weight))
+
+    bmi = weight / (height * height)
+    print("BMI= ", str(bmi))
+
+    if bmi<18.5:
+        print("Underweight")
+    elif bmi>=18.5 and bmi<=25.0:
+        print("Normal Weight")
+    else:
+        print("Overweight")
+
+calculate_bmi(1.77, 75)
